53c0bb3d0991085aebb502fa66492f8b9a7dd193
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 22:26:27 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,10 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
+def directors_database
+	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
+  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
+end
 
 def pretty_print_nds(nds)
   pp nds
 end
 
 def print_first_directors_movie_titles
+
 end
